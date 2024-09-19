a518d0ca409c6840f6a089719af8412c6024089c
Giacomo <hello@0xjei.dev>
2024-09-19 22:21:24 +0200

    refactor: add cover and generate again button

--- pages/index.js
+++ pages/index.js
@@ -22,13 +22,7 @@ import {
   DropdownMenuPortal,
   DropdownMenuSubContent,
 } from "@/components/ui/dropdown-menu";
-import {
-  UserPlus,
-  Mail,
-  MessageSquare,
-  DropdownMenuSeparator,
-  PlusCircle,
-} from "lucide-react";
+import bgCover from "../assets/generator.jpg";
 
 // Function to read and parse the JSON file
 const readIdeasFile = async (filename) => {
@@ -127,147 +121,158 @@ export default function Home() {
 
   return (
     <main className="bg-black w-full min-h-screen pb-10">
-      <nav className="flex justify-between p-5 items-center bg-[#0d0d0d]">
-        <img
-          className="inline-flex justify-center w-40"
-          alt="Web3PrivacyNow"
-          src="https://web3privacy.info/logo.svg"
-        />
-        <a href={contributeIdeaTo} target="_blank" rel="noopener noreferrer">
-          <button className="border archivo text-xs flex space-x-3 items-center p-2">
-            <h3>CONTRIBUTE AN IDEA</h3>
-            <Github className="w-4" />
-          </button>
-        </a>
-      </nav>
-      <div className="bg-[#0d0d0d] h-96 pt-20">
-        <h1 className="major text-center text-3xl md:text-5xl">
-          privacy idea generator
-        </h1>
-        <h3 className="archivo text-center text-[#4c4c4c] mt-5 px-5 md:text-xl">
-          Generate ideas curated by the Web3Privacy Now community and experts
-        </h3>
-        <div className="flex justify-center mt-10 md:mt-20 space-x-4">
-          <Dialog>
-            <DialogTrigger>
-              <div
-                className="bg-white text-[#0d0d0d] archivo text-sm flex space-x-3 items-center p-2"
-                onClick={handleGenerateIdea}
-              >
-                <h3>GENERATE IDEA</h3>
-              </div>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogDescription className="">
-                  {isLoading ? (
-                    <p>Loading ideas...</p>
-                  ) : error ? (
-                    <p>{error}</p>
-                  ) : generatedIdea ? (
-                    <>
-                      <div className="h-10">
-                        <h1 className="archivo text-lg mb-4">
-                          {generatedIdea.name}
-                        </h1>
-                      </div>
-                      <hr className="opacity-10 my-2" />
-                      <div className="h-20">
-                        <h3 className="archivo mt-4 text-sm opacity-50">
-                          {generatedIdea.description}
+      <div className="bg-image-class p-5" style={{ backgroundImage: `url(${bgCover.src})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+        <nav className="flex justify-between p-5 items-center">
+          <img
+            className="inline-flex justify-center w-40"
+            alt="Web3PrivacyNow"
+            src="https://web3privacy.info/logo.svg"
+          />
+          <a href={contributeIdeaTo} target="_blank" rel="noopener noreferrer">
+            <button className="border archivo text-xs flex space-x-3 items-center p-2">
+              <h3>CONTRIBUTE AN IDEA</h3>
+              <Github className="w-4" />
+            </button>
+          </a>
+        </nav>
+        <div>
+          <h1 className="major text-center text-3xl md:text-5xl">
+            privacy idea generator
+          </h1>
+          <h3 className="archivo text-center text-[#4c4c4c] mt-5 px-5 md:text-xl">
+            Generate ideas curated by the Web3Privacy Now community and experts
+          </h3>
+          <div className="flex justify-center mt-10 md:mt-20 space-x-4">
+            <Dialog>
+              <DialogTrigger>
+                <div
+                  className="bg-white text-[#0d0d0d] archivo text-sm flex space-x-3 items-center p-2"
+                  onClick={handleGenerateIdea}
+                >
+                  <h3>GENERATE IDEA</h3>
+
+                </div>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogDescription className="">
+                    {isLoading ? (
+                      <p>Loading ideas...</p>
+                    ) : error ? (
+                      <p>{error}</p>
+                    ) : generatedIdea ? (
+                      <>
+                        <div className="h-10">
+                          <h1 className="archivo text-lg mb-4">
+                            {generatedIdea.name}
+                          </h1>
+                        </div>
+                        <hr className="opacity-10 my-2" />
+                        <div className="h-20">
+                          <h3 className="archivo mt-4 text-sm opacity-50">
+                            {generatedIdea.description}
+                          </h3>
+                        </div>
+                        <div className="flex justify-between items-center my-3">
+                          <div className="flex space-x-3 w-2/3">
+                            {generatedIdea.categories.map((category, index) => (
+                              <Badge key={index} variant="secondary">
+                                {category}
+                              </Badge>
+                            ))}
+                          </div>
+                          <div className="w-1/3 text-xs text-end flex justify-end space-x-3">
+                            {generatedIdea.github && (
+                              <a
+                                href={generatedIdea.github}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                <Github className="w-5 h-5" />
+                              </a>
+                            )}
+                            {generatedIdea.website && (
+                              <a
+                                href={generatedIdea.website}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                <Globe className="w-5 h-5" />
+                              </a>
+                            )}
+                          </div>
+                        </div>
+                        <hr className="opacity-10 my-3" />
+                        <h3 className="archivo text-lg mb-4 text-center mx-auto">
+                          {generatedIdea.event}
                         </h3>
-                      </div>
-                      <div className="flex justify-between items-center my-3">
-                        <div className="flex space-x-3 w-2/3">
-                          {generatedIdea.categories.map((category, index) => (
-                            <Badge key={index} variant="secondary">
-                              {category}
-                            </Badge>
-                          ))}
+                        <div className="flex justify-center">
+                          <button
+                            className="bg-white text-[#0d0d0d] archivo text-sm flex space-x-3 items-center p-2 mt-4"
+                            onClick={handleGenerateIdea}
+                          >
+                            GENERATE AGAIN
+                          </button>
                         </div>
-                        <div className="w-1/3 text-xs text-end flex justify-end space-x-3">
-                          {generatedIdea.github && (
-                            <a
-                              href={generatedIdea.github}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <Github className="w-5 h-5" />
-                            </a>
-                          )}
-                          {generatedIdea.website && (
-                            <a
-                              href={generatedIdea.website}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <Globe className="w-5 h-5" />
-                            </a>
-                          )}
-                        </div>
+                      </>
+                    ) : (
+                      <div>
+                        Looks like no ideas were found for the specified filters
                       </div>
-                      <hr className="opacity-10 my-3" />
-                      <h3 className="archivo mt-2 text-sm opacity-50 text-end hover:underline cursor-pointer">
-                        {generatedIdea.event}
-                      </h3>
-                    </>
-                  ) : (
-                    <div>
-                      Looks like no ideas were found for the specified filters
-                    </div>
-                  )}
-                </DialogDescription>
-              </DialogHeader>
-            </DialogContent>
-          </Dialog>
-          <DropdownMenu>
-            <DropdownMenuTrigger className="archivo text-sm flex space-x-3 items-center p-2">
-              <Filter className="text-white" />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-72">
-              <DropdownMenuSub>
-                <DropdownMenuSubTrigger>
-                  Filter by type (
-                  {selectedIdeaType === "community"
-                    ? "Community Ideas"
-                    : "Expert Ideas"}
-                  )
-                </DropdownMenuSubTrigger>
-                <DropdownMenuPortal>
-                  <DropdownMenuSubContent>
-                    <DropdownMenuItem
-                      onClick={() => setSelectedIdeaType("community")}
-                    >
-                      Community Ideas
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      onClick={() => setSelectedIdeaType("expert")}
-                    >
-                      Expert Ideas
-                    </DropdownMenuItem>
-                  </DropdownMenuSubContent>
-                </DropdownMenuPortal>
-              </DropdownMenuSub>
-              <DropdownMenuSub>
-                <DropdownMenuSubTrigger>
-                  Filter by tags ({selectedTags.length})
-                </DropdownMenuSubTrigger>
-                <DropdownMenuPortal>
-                  <DropdownMenuSubContent className="max-h-60 overflow-y-auto">
-                    {allTags.map((tag) => (
-                      <DropdownMenuCheckboxItem
-                        key={tag}
-                        checked={selectedTags.includes(tag)}
-                        onCheckedChange={() => handleTagToggle(tag)}
+                    )}
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="archivo text-sm flex space-x-3 items-center p-2">
+                <Filter className="text-white" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-72">
+                <DropdownMenuSub>
+                  <DropdownMenuSubTrigger>
+                    Filter by type (
+                    {selectedIdeaType === "community"
+                      ? "Community Ideas"
+                      : "Expert Ideas"}
+                    )
+                  </DropdownMenuSubTrigger>
+                  <DropdownMenuPortal>
+                    <DropdownMenuSubContent>
+                      <DropdownMenuItem
+                        onClick={() => setSelectedIdeaType("community")}
                       >
-                        {tag}
-                      </DropdownMenuCheckboxItem>
-                    ))}
-                  </DropdownMenuSubContent>
-                </DropdownMenuPortal>
-              </DropdownMenuSub>
-            </DropdownMenuContent>
-          </DropdownMenu>
+                        Community Ideas
+                      </DropdownMenuItem>
+                      <DropdownMenuItem
+                        onClick={() => setSelectedIdeaType("expert")}
+                      >
+                        Expert Ideas
+                      </DropdownMenuItem>
+                    </DropdownMenuSubContent>
+                  </DropdownMenuPortal>
+                </DropdownMenuSub>
+                <DropdownMenuSub>
+                  <DropdownMenuSubTrigger>
+                    Filter by tags ({selectedTags.length})
+                  </DropdownMenuSubTrigger>
+                  <DropdownMenuPortal>
+                    <DropdownMenuSubContent className="max-h-60 overflow-y-auto">
+                      {allTags.map((tag) => (
+                        <DropdownMenuCheckboxItem
+                          key={tag}
+                          checked={selectedTags.includes(tag)}
+                          onCheckedChange={() => handleTagToggle(tag)}
+                        >
+                          {tag}
+                        </DropdownMenuCheckboxItem>
+                      ))}
+                    </DropdownMenuSubContent>
+                  </DropdownMenuPortal>
+                </DropdownMenuSub>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
       </div>
       <div className="mt-10">
